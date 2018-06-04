467e58517920d011e1eaba40ee6ce6044047f29d
DESKTOP-LKJ5344\M Bagus Santoso <muhamadbagussantoso@gmail.com>
2018-06-04 11:17:12 +0700

    udah truktur template

--- application/libraries/template.php
+++ application/libraries/template.php
@@ -8,10 +8,10 @@ class Template
             $this->_ci = &get_instance();
             $this->dirLoad = array('public','templates','front','admin','user','manage','report');
             $this->fileTemplates = array('header','navbar','body','footer');
-            $this->adminTemplate = 'front/admin/templates';
+            $this->adminTemplate = 'front/templates';
             $this->userTemplate = 'front/user/templates';
             $this->subdirLoad = array('goods','invoice');
-            $this->adminDir = 'front/admin';
+            $this->adminDir = 'front';
          }
          
         function display($templates,$data = null)
--- application/views/front/User/dashboard_user.php
+++
@@ -1,5 +0,0 @@
-
-
-Hai <b><?php echo $user['user_name'] ; echo $auth ;?>
-</b> horeee kamu berhasil login. Ini halaman user. Udah masuk mau keluar? nih keluarin disini >
- [<a href='<?php echo base_url('dashboard/logout');?>'>Keluar</a>]
